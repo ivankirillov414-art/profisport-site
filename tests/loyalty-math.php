e127f0f0e17d2861ddfe2000da7ec1ac057bb831
ivankirillov414-art <ivankirillov414@gmail.com>
2026-09-26 13:49:35 +0500

    ProfiSport: extend loyalty math test for calculator options

--- tests/loyalty-math.php
+++ tests/loyalty-math.php
@@ -6,6 +6,11 @@ $default=loyalty_default_config();
 if($default['enabled']!==false||loyalty_configured($default)!==false)throw new RuntimeException('Default loyalty config must be disabled and incomplete');
 
 $config=$default;
+$config['earn_enabled']=true;
+$config['redeem_enabled']=true;
+$config['expiration_enabled']=true;
+$config['review_bonus_enabled']=true;
+$config['category_exclusions_enabled']=true;
 $config['earn_percent_bp']=500;
 $config['max_redeem_percent_bp']=3000;
 $config['point_value_kopeks']=100;
@@ -20,9 +25,11 @@ $preview=loyalty_order_earn_preview([
   ['line_total_rub'=>2000,'category_path'=>'Подарочные сертификаты / Электронные'],
 ],$config);
 if($preview['eligible_rub']!==10000||$preview['points']!==500)throw new RuntimeException('Loyalty earn preview is incorrect');
+$redeem=loyalty_redemption_preview(10000,2000,$config);
+if($redeem['max_points']!==2000||$redeem['discount_rub']!==2000||$redeem['payable_rub']!==8000)throw new RuntimeException('Loyalty redemption preview is incorrect');
 
 $config['min_order_rub']=20000;
 $preview=loyalty_order_earn_preview([['line_total_rub'=>10000,'category_path'=>'Велосипеды']],$config);
 if($preview['points']!==0)throw new RuntimeException('Minimum order rule is incorrect');
 
-echo "PASS: loyalty config safety, category exclusions and earn math\n";
+echo "PASS: loyalty config safety, feature switches, category exclusions, earn and redemption math\n";
